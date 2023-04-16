e833752c4e02d65e288d753fe142bdb0c2ef869e
Sebastian <sebastian.neumair@gmail.com>
2023-04-16 22:38:32 +0200

    add noremap = true to gitsign keymaps

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -22,17 +22,19 @@ require('gitsigns').setup{
     end, {expr=true})
 
     -- Actions
+    local opts = { noremap = true  }
+
     map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
     map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-    map('n', '<leader>hS', gs.stage_buffer)
-    map('n', '<leader>hu', gs.undo_stage_hunk)
-    map('n', '<leader>hR', gs.reset_buffer)
-    map('n', '<leader>hp', gs.preview_hunk)
-    map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-    map('n', '<leader>tb', gs.toggle_current_line_blame)
-    map('n', '<leader>hd', gs.diffthis)
-    map('n', '<leader>hD', function() gs.diffthis('~') end)
-    map('n', '<leader>td', gs.toggle_deleted)
+    map('n', '<leader>hS', gs.stage_buffer, opts)
+    map('n', '<leader>hu', gs.undo_stage_hunk, opts)
+    map('n', '<leader>hR', gs.reset_buffer, opts)
+    map('n', '<leader>hp', gs.preview_hunk, opts)
+    map('n', '<leader>hb', function() gs.blame_line{full=true} end, opts)
+    map('n', '<leader>tb', gs.toggle_current_line_blame, opts)
+    map('n', '<leader>hd', gs.diffthis, opts)
+    map('n', '<leader>hD', function() gs.diffthis('~') end, opts)
+    map('n', '<leader>td', gs.toggle_deleted, opts)
 
     -- Text object
     map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
