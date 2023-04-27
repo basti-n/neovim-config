46f0ed255cb96aef4e5aa9be5ad6983272f8eb6b
Sebastian Neumair <sebastian.neumair@gmail.com>
2023-04-27 09:25:06 +0200

    trigger cmp suggestion without typing (<C-Space>)

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -37,7 +37,8 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-  ["<C-Space>"] = cmp.mapping.complete(),
+  -- show suggestion before typing
+  ['<C-Space>'] = cmp.mapping(cmp.mapping.complete({}), { 'i', 'c' }),
 })
 
 cmp_mappings['<Tab>'] = nil
