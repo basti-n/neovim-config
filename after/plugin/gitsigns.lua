669e3bd343bd8a6d9cc0cb437f6ea79ac386ca9c
Sebastian <sebastian.neumair@gmail.com>
2023-07-30 16:46:40 +0200

    gitsigns remove duplicate map call

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -15,12 +15,6 @@ require('gitsigns').setup{
       return '<Ignore>'
     end, {expr=true})
 
-    map('n', '[c', function()
-      if vim.wo.diff then return '[c' end
-      vim.schedule(function() gs.prev_hunk() end)
-      return '<Ignore>'
-    end, {expr=true})
-
     -- Actions
     local opts = { noremap = true  }
 
